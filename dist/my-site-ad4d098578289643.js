ec2024898ed44ac40dacc0e211c0d9eb5bb3e366
mate <matekopaliani12@gmail.com>
2025-12-24 01:00:01 +0400

    update base page

--- dist/my-site-ad4d098578289643.js
+++ dist/my-site-ad4d098578289643.js
@@ -714,15 +714,15 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper3957 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3955 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 471, __wbg_adapter_40);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper6421 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper6419 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 568, __wbg_adapter_43);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper6896 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper6894 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 585, __wbg_adapter_46);
         return ret;
     };
